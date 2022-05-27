4a035753a2d701e2acb7333b98626fb0b52dc274
Ignacio Sagues <isagues@itba.edu.ar>
2022-05-27 19:08:40 -0300

    Gaussian fitting

--- tracking/main.py
+++ tracking/main.py
@@ -5,14 +5,12 @@ from enum import Enum
 from typing import List, Optional
 
 import numpy as np
-import skimage as skimg
 
 import tracking.image_utils as image_utils
-from tracking.tracking import adjust_points
+from tracking.tracking import adjust_points, multi_point_linear_interpolation
 from tracking.types_utils import Point
 
 SAMPLE_POINTS = 100
-PIXEL_POINT_RATIO = 1
 
 # TODO(tobi): Not used. Find use or deprecate. It could be usefull to store point data.
 def save_info_as_json(folder: str, points: List[Point], frame_name: str) -> None:
@@ -50,14 +48,6 @@ def save_results(folder, img, points: np.ndarray, frame: str, normal_lines: Opti
     image_utils.save_plot(folder, frame_name)
     # save_info_as_json(folder, points, frame_name)
 
-def points_linear_interpolation(points: np.ndarray, pixel_point_ratio: int) -> np.ndarray:
-    """
-    Given a point vector, interpolates linearly between each point pair.
-    """
-    # line returns the pixels of the line described by the 2 points
-    # https://scikit-image.org/docs/stable/api/skimage.draw.html#line
-    # Nota(tobi): Aca hay una bocha de truquito, lo podemos simplificar
-    return np.concatenate([np.stack(skimg.draw.line(*start, *end), axis=-1) for start, end in zip(points, points[1:])])
 
 class TrackStep(Enum):
     INTERPOLATION   = "interpolation"
@@ -93,17 +83,16 @@ def track_filament(frames_folder: str, user_points: np.ndarray, up_to_step: Trac
     start_frame = frames[0]
     img, invert = image_utils.get_frame(start_frame)
 
-    starting_points = points_linear_interpolation(user_points, PIXEL_POINT_RATIO)
     if up_to_step == TrackStep.INTERPOLATION:
-        save_results(frames_folder, img, starting_points, start_frame, scatter=True)
+        save_results(frames_folder, img, multi_point_linear_interpolation(user_points), start_frame, scatter=True)
         return
 
     # TODO(tobi): Que normal_len sea un parametro, y que sino se pueda calcular a partir del ancho del tubo en esa seccion
     normal_len = 10
-    normal_line_resolution = 15
-    normal_lines = None
+    angle_resolution = 15
+    normal_lines_bounds = None
     scatter = up_to_step != TrackStep.NORMAL
-    prev_frame_points = starting_points
+    prev_frame_points = user_points
 
     for i, frame in enumerate(frames):
         img, _ = image_utils.get_frame(frame, invert)
@@ -112,36 +101,12 @@ def track_filament(frames_folder: str, user_points: np.ndarray, up_to_step: Trac
         if up_to_step == TrackStep.FILTER:
             img = blurred_img
             prev_frame_points = None
-        elif up_to_step == TrackStep.NORMAL:
-            normal_lines = generate_normal_line_bounds(prev_frame_points, normal_line_resolution, normal_len)
 
         elif up_to_step == TrackStep.ALL:
-            prev_frame_points = adjust_points(blurred_img, prev_frame_points, normal_len)
+            interpolated_points = multi_point_linear_interpolation(prev_frame_points)
+            prev_frame_points, normal_lines_bounds = adjust_points(img, interpolated_points, normal_len, angle_resolution)
 
-        save_results(frames_folder, img, prev_frame_points, frame, normal_lines=normal_lines,scatter=scatter)
-
-
-def generate_normal_line_bounds(points: np.ndarray, resolution: int, line_len: float) -> np.ndarray:
-    
-    d = resolution
-
-    start = points[:-d]
-    end = points[d:]
-
-    section_angle = np.arctan2(end[:,1] - start[:,1], end[:,0] - start[:,0])
-    
-    normal_angle = np.zeros(len(points))
-    normal_angle[d//2:-d//2] = section_angle + np.pi/2
-    normal_angle[:d//2] = normal_angle[d//2]
-    normal_angle[-d//2:] = normal_angle[-d//2 -1]
-
-    component_multiplier = np.stack((np.cos(normal_angle), np.sin(normal_angle)), axis=1)
-    upper = points + component_multiplier * line_len / 2
-    lower = points - component_multiplier * line_len / 2
-    
-    bounds = np.stack((upper, lower), axis=1)
-
-    return np.rint(bounds).astype(np.int64)
+        save_results(frames_folder, img, prev_frame_points, frame, normal_lines=normal_lines_bounds, scatter=scatter)
 
 def save_tracking_film(frames_folder) -> str:
     results_folder = f'{frames_folder}/results'
--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,10 +1,14 @@
-from typing import List, Union
 import math
+from itertools import tee
+from typing import List, Tuple, Union
+
 import numpy as np
+import skimage as skimg
+from scipy.optimize import curve_fit
 
 from tracking.types_utils import Point
-from itertools import tee
 
+PIXEL_POINT_RATIO=1
 
 def circle_coordinate(start: Point, angle: float, radius: float) -> Point:
     """
@@ -15,113 +19,71 @@ def circle_coordinate(start: Point, angle: float, radius: float) -> Point:
     y = start.y + math.sin(angle) * radius
     return Point(int(x), int(y))
 
-def threes(iterator):
-    """
-        Given the list [s0, s1, ...], returns an iterator that returns sets
-         of three consecutive values. Ex: (s0,s1,s2), (s1,s2,s3), ...
-    """
-    a, b, c = tee([None] + iterator + [None], 3)
-    next(b, None)
-    next(c, None)
-    next(c, None)
-    return zip(a, b, c)
 
-
-def get_line_angle(p1: Point, p2: Point) -> Union[float, None]:
-    """
-        Returns the angle between line from (p1.x, p1.y) -> (p2.x, p1.y) and
-         the line formed by p1 and p2.
-        (angles start at 0 to the right and get bigger in the
-         clockwise direction)
-    """
-    if p1 is None or p2 is None:
-        a = None
-    else:
-        a = math.atan2((p2.y-p1.y), (p2.x-p1.x))
-
-    return a
-
-
-def get_line_points(start_point: Point, amount: int, length: float, angle: float) -> List:
-    """
-        Returns `amount` equidistant points along the line described by `angle`, `length` and `start_point`
-        (along with each point's distance to the center)
-    """
-    # TODO(WARNING): no son siempre centrados. Ej para un largo 10 y amount 5 devuelve [0,2,4,6,8]
-    #  No deberia ser problema con amount grandes como se piensa usar (>100)
-    lengths = [(a/amount-1/2)*length for a in range(amount)]
-    
-    return [{'point': circle_coordinate(start_point, angle, l), 'dist': l} for l in lengths]
-
-
-def bright_score(point: Point, img, dist, max_len) -> float:
-    bright_weight = 0.75
-    br_score = point.brightness(img)/255.0
-    dst_score = 1-abs(dist / max_len)
-    return br_score * bright_weight + dst_score * (1 - bright_weight)
-
-
-def get_brightest_point(start_point: Point, amount: int, length: float, angle: float, img) -> Point:
-    line_points = get_line_points(start_point, amount, length, angle)
-
-    brightest = {'point': start_point, 'score': bright_score(start_point, img, 0, length/2)}
-    for p in line_points:
-        point_score = bright_score(p['point'], img, p['dist'], length/2)
-        if point_score > brightest['score']:
-            brightest = {'point': p['point'], 'score': point_score}
-    return brightest['point']
-
-
-def adjust_point(point: Point, test_points_amount: int, recursion_depth: int, normal_len: float, normal_angle: float, img) -> Point:
-    """
-        Return brightest point along the normal line.
-        Steps:
-            1. Create an intensity profile for the normal line (grab equidistant points and record their brightness)
-            2. Select the brightest point
-            3. Optional recursive step: Repeat 1 and 2 starting from the point selected at 2 and with a smaller length
-    """
-    brightest_point = point
-    
-    for _ in range(recursion_depth):
-        brightest_point = get_brightest_point(brightest_point, test_points_amount, normal_len, normal_angle, img)
-        normal_len /= test_points_amount
-    
-    return brightest_point
-
-def adjust_points(img, points: np.ndarray, normal_len: float) -> np.ndarray:
-    """
-        Steps. For each point:
-            1 Find normal (tangent + 90 degrees) of line
-            2 From a normal line of length `normal_len` centered on the filament line test the brightness
-                of `test_points_amount` points
-            3 From the brightest point, start another local search to again find the brightness point
-            4 Repeat steps 2 and 3 `search_iterations` times
-            5 At the end of the search, append the new point to new_points
-    """
+def adjust_points(img, points: np.ndarray, normal_len: float, angle_resolution: int) -> np.ndarray:
     # TODO(tobi): normal_len deberia estar estar expresado en pixeles??
-    # Configuration
-    # TODO(FIP6): find way to calculate velocity using previous movements and have normal_len depend on it (bigger velocity, bigger normal_len)
-    #  This would help avoid losing a fast moving the filament.
-    #  Alternative: using the velocity make a better guess of the next filament center and start searching there (don't change normal_len)
-
     # TODO(tobi): pasar el algoritmo a ndarray. Por ahora hacemos la traduccion de manera interna
-    points = [Point(x, y) for x, y in points]
+    
+    normal_lines_bounds = generate_normal_line_bounds(points, angle_resolution, normal_len)
+    # No todas salen con la misma longitud
+    normal_lines = [points_linear_interpolation(start, end) for start, end in normal_lines_bounds]
+    intensity_profiles = map(lambda nl: img[nl[:,1], nl[:,0]], normal_lines)
+    brightest_point_profile_index = map(lambda ip: gauss_fitting(ip, img.max())[0], intensity_profiles)
+    brightest_point = np.array([index_to_point(idx, nl) for idx, nl in zip(brightest_point_profile_index, normal_lines)])
+    
+    # TODO: ver como y/o cuando se guardan los valores precisos, mientras tanto se trabaja con valores discretos
+    return np.rint(brightest_point).astype(int), normal_lines_bounds
 
-    test_points_amount = 250
-    recursion_depth = 1
 
-    new_points = []
-    for left_p, p, right_p in threes(points):
-        if left_p is None:
-            normal_angle = get_line_angle(p, right_p) + math.pi/2
-        elif right_p is None:
-            normal_angle = get_line_angle(p, left_p) + math.pi/2
-        else:
-            # TODO(tobi): Yo haria el promedio entre [left_p, p] y [p, right_p]
-            normal_angle = get_line_angle(left_p, right_p) + math.pi/2
+def multi_point_linear_interpolation(points: np.ndarray, pixel_point_ratio: int = PIXEL_POINT_RATIO) -> np.ndarray:
+    """
+    Given a point vector, interpolates linearly between each point pair.
+    """
+    return np.append(np.concatenate([points_linear_interpolation(start, end)[:-1] for start, end in zip(points, points[1:])]), [points[-1]], axis=0)
 
-        new_point = adjust_point(p, test_points_amount, recursion_depth, normal_len, normal_angle, img)
-        new_points.append(new_point)
+def points_linear_interpolation(start: Tuple[int, int], end: Tuple[int, int]) -> np.ndarray:
+    # line returns the pixels of the line described by the 2 points
+    # https://scikit-image.org/docs/stable/api/skimage.draw.html#line
+    # Nota(tobi): Aca hay una bocha de truquito, lo podemos simplificar
+    return np.stack(skimg.draw.line(*start, *end), axis=-1)
 
-    return np.asarray([(p.x, p.y) for p in new_points])
+def index_to_point(idx: float, points: np.ndarray) -> Tuple[float, float]:
+    start = points[int(idx)]
+    end = points[int(idx)+1]
+    delta = idx - int(idx)
 
+    new_x = start[0] + delta * (end[0] - start[0])
+    new_y = start[1] + delta * (end[1] - start[1])
+
+    return new_x, new_y
+
+def gaussian(x, mu, sig):
+    return 1./(np.sqrt(2.*np.pi)*sig)*np.exp(-np.power((x - mu)/sig, 2.)/2)
+
+def gauss_fitting(intensity_profile: np.ndarray, max_color: int) -> np.ndarray:
+    xdata = np.arange(len(intensity_profile))
+    popt, _ = curve_fit(lambda x, m, s: gaussian(x, m, s) * max_color, xdata, intensity_profile)
+
+    return popt
+
+def generate_normal_line_bounds(points: np.ndarray, angle_resolution: int, normal_len: float) -> np.ndarray:
+    
+    d = angle_resolution
+
+    start = points[:-d]
+    end = points[d:]
+
+    section_angle = np.arctan2(end[:,1] - start[:,1], end[:,0] - start[:,0])
+    
+    normal_angle = np.zeros(len(points))
+    normal_angle[d//2:-d//2] = section_angle + np.pi/2
+    normal_angle[:d//2] = normal_angle[d//2]
+    normal_angle[-d//2:] = normal_angle[-d//2 -1]
+
+    component_multiplier = np.stack((np.cos(normal_angle), np.sin(normal_angle)), axis=1)
+    upper = points + component_multiplier * normal_len / 2
+    lower = points - component_multiplier * normal_len / 2
+    
+    bounds = np.stack((upper, lower), axis=1)
+
+    return np.rint(bounds).astype(np.int64)
