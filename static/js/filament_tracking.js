0d55fd90bee03894e88d01c8047bf84217393354
Tobias Brandy <tbrandy@itba.edu.ar>
2022-07-11 00:45:03 -0300

    Frontend: uncouple image parsing

--- static/js/filament_tracking.js
+++ static/js/filament_tracking.js
@@ -5,13 +5,13 @@ const LINE_WIDTH        = 5;
 const POINT_COLOR       = '#ff0000';
 
 // Elementos de UI
-const canvas        = document.getElementById('points-selector-canvas');
-const form          = document.getElementById('tracking-form');
-const imgSelector   = document.getElementById('img-selector');
-const errors        = document.getElementById('errors');
-const undo          = document.getElementById('undo');
-const redo          = document.getElementById('redo');
-const resultImgs    = document.getElementById('result-imgs');
+const selectorCanvas    = document.getElementById('points-selector-canvas');
+const form              = document.getElementById('tracking-form');
+const imgSelector       = document.getElementById('img-selector');
+const errors            = document.getElementById('errors');
+const undo              = document.getElementById('undo');
+const redo              = document.getElementById('redo');
+const resultImgs        = document.getElementById('result-imgs');
 
 /* ------ Global data -------- */
 // Points info
@@ -19,18 +19,16 @@ const selected_points   = [];
 const redo_points       = [];
 
 // Image and canvas info
-let imageData;
-let img_w;
-let img_h;
+let selectorDrawable;
 
 (function () {
     form.addEventListener('submit', executeTracking);
 
     imgSelector.addEventListener('change', handleImageSelection);
 
-    canvas.addEventListener('click', onCanvasClick);
-    canvas.style.display = 'none';
-    canvas.width = CANVAS_RESOLUTION;
+    selectorCanvas.addEventListener('click', onCanvasClick);
+    selectorCanvas.style.display = 'none';
+    selectorCanvas.width = CANVAS_RESOLUTION;
 
     // Undo/Redo selected points
     undo.addEventListener('click', undoPoint);
@@ -38,23 +36,63 @@ let img_h;
 })();
 
 function executeTracking(e) {
+    e.preventDefault();
+
     if(selected_points.length < 2) {
         errors.innerText = 'Debe seleccionar el punto inicial y final del filamento';
-    } else {
-        // TODO(tobi): Request del form mediante fetch api. Incluirle los selected points.
+        return;
     }
 
-    e.preventDefault();
+    // TODO(tobi): Request del form mediante fetch api. Incluirle los selected points.
+}
+
+// In case of error returns error message
+async function* drawableIterator(images) {
+    // Files is not iterable
+    for(const image of images) {
+
+        // TODO(tobi): Agregar soporte para avi, png, jpg, etc
+        if(image.type === 'image/tiff') {
+            const buffer = await image.arrayBuffer();
+            const ifds = UTIF.decode(buffer);
+            for(const ifd of ifds) {
+                UTIF.decodeImage(buffer, ifd, ifds);
+
+                const rgbaData = UTIF.toRGBA8(ifd);
+                const imageData = new ImageData(new Uint8ClampedArray(rgbaData), ifd.width, ifd.height);
+
+                const drawable = document.createElement('canvas');
+                drawable.width = ifd.width;
+                drawable.height = ifd.height;
+
+                // Rendereamos la imagen en un canvas intermedio para luego poder escalar la imagen
+                const ctx = drawable.getContext('2d');
+                ctx.putImageData(imageData, 0, 0, 0, 0, ifd.width, ifd.height);
+
+                yield drawable;
+            }
+        } else {
+            // Ignoramos tipos que no conocemos
+        }
+    }
+}
+
+function showError(message) {
+    errors.innerText = message;
+}
+
+function clearError() {
+    errors.innerText = '';
 }
 
 function canvasPos2Pixel(pos, canvas_len, img_len) {
     return Math.trunc(pos / canvas_len * img_len)
 }
 function canvasPos2PixelX(x, canvas_w) {
-    return canvasPos2Pixel(x, canvas_w, img_w);
+    return canvasPos2Pixel(x, canvas_w, selectorDrawable.width);
 }
 function canvasPos2PixelY(y, canvas_h) {
-    return canvasPos2Pixel(y, canvas_h, img_h);
+    return canvasPos2Pixel(y, canvas_h, selectorDrawable.height);
 }
 
 function pixel2CanvasPos(pixel, canvas_draw_len, img_len) {
@@ -62,14 +100,14 @@ function pixel2CanvasPos(pixel, canvas_draw_len, img_len) {
     return (pixel + 0.5) / img_len * canvas_draw_len;
 }
 function pixel2CanvasPosX(x) {
-    return pixel2CanvasPos(x, canvas.width, img_w);
+    return pixel2CanvasPos(x, selectorCanvas.width, selectorDrawable.width);
 }
 function pixel2CanvasPosY(y) {
-    return pixel2CanvasPos(y, canvas.height, img_h);
+    return pixel2CanvasPos(y, selectorCanvas.height, selectorDrawable.height);
 }
 
 function onCanvasClick(event) {
-    let rect = canvas.getBoundingClientRect();
+    let rect = selectorCanvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
 
@@ -94,7 +132,7 @@ function addPoint(point) {
 }
 
 function drawPoint(point) {
-    let ctx = canvas.getContext("2d");
+    let ctx = selectorCanvas.getContext("2d");
 
     let x = pixel2CanvasPosX(point.x);
     let y = pixel2CanvasPosY(point.y);
@@ -118,25 +156,17 @@ function drawPoint(point) {
 }
 
 function undoPoint() {
-    let ctx = canvas.getContext("2d");
+    let ctx = selectorCanvas.getContext("2d");
 
     if(selected_points.length > 0) {
         redo_points.push(selected_points.pop());
 
         // Clean canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, selectorCanvas.width, selectorCanvas.height);
 
         // Redraw image
-        // TODO(tobi): Pasarlo a una funcion auxiliar
-        const tmpCanvas = document.createElement('canvas');
-        tmpCanvas.height = img_h;
-        tmpCanvas.width = img_w;
-
-        const tmpCtx = tmpCanvas.getContext('2d');
-        tmpCtx.putImageData(imageData, 0, 0, 0, 0, img_w, img_h);
-
         canvasDisableSmoothing(ctx);
-        ctx.drawImage(tmpCanvas, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(selectorDrawable, 0, 0, selectorCanvas.width, selectorCanvas.height);
 
         // Redraw all poins
         selected_points.forEach(drawPoint)
@@ -154,7 +184,7 @@ function redoPoint() {
 function updateInterface() {
     undo.style.visibility = selected_points.length === 0 ? 'hidden' : 'visible';
     redo.style.visibility = redo_points.length === 0 ? 'hidden' : 'visible';
-    errors.innerText = '';
+    clearError();
 }
 
 function canvasDisableSmoothing(ctx) {
@@ -167,47 +197,33 @@ function canvasDisableSmoothing(ctx) {
 
 // Empty selected points and build canvas for point selection
 async function handleImageSelection() {
-    if(imgSelector.length === 0) {
+    if(imgSelector.files.length === 0) {
           // No nos subieron nada
-          errors.innerText = 'No image selected';
+          showError('No images selected');
+          imgSelector.value = '';
           return;
     }
 
-  let first = imgSelector.files[0];
+    const firstDrawable = await drawableIterator(imgSelector.files).next();
+    if(!firstDrawable || firstDrawable.done) {
+        showError('No valid image selected');
+        imgSelector.value = '';
+        return;
+    }
 
-  if(first.type === 'image/tiff') {
-        const buffer = await first.arrayBuffer();
-        const ifds = UTIF.decode(buffer);
-    const ifd = ifds[0];
-    UTIF.decodeImage(buffer, ifd, ifds);
+    // Get selector drawable value
+    selectorDrawable = firstDrawable.value;
 
-    const rgbaData = UTIF.toRGBA8(ifd);
-    imageData = new ImageData(new Uint8ClampedArray(rgbaData), ifd.width, ifd.height);
+    // Update selector canvas with new drawable
+    selectorCanvas.height = selectorCanvas.width / selectorDrawable.width * selectorDrawable.height;
+    let ctx = selectorCanvas.getContext('2d');
+    canvasDisableSmoothing(selectorCanvas.getContext('2d'));
+    ctx.drawImage(selectorDrawable, 0, 0, selectorCanvas.width, selectorCanvas.height);
 
-    img_w = ifd.width;
-    img_h = ifd.height;
-    canvas.height = canvas.width / img_w * img_h;
+    // Show canvas
+    selectorCanvas.style.display = '';
 
-    const tmpCanvas = document.createElement('canvas');
-    tmpCanvas.height = img_h;
-    tmpCanvas.width = img_w;
-
-    // Rendereamos la imagen en un canvas intermedio para luego poder escalar la imagen
-    const tmpCtx = tmpCanvas.getContext('2d');
-    tmpCtx.putImageData(imageData, 0, 0, 0, 0, img_w, img_h);
-
-    let ctx = canvas.getContext('2d');
-    canvasDisableSmoothing(canvas.getContext('2d'));
-    ctx.drawImage(tmpCanvas, 0, 0, canvas.width, canvas.height);
-  } else {
-    errors.innerText = 'Image type not supports yet';
-    return;
-  }
-
-  // Show canvas
-  canvas.style.display = '';
-
-  // Reset selected points
-  selected_points.length    = 0;
-  redo_points.length        = 0;
+    // Reset selected points
+    selected_points.length    = 0;
+    redo_points.length        = 0;
 }
