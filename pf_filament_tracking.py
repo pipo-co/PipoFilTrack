8233b781fe2afa31ab3d4f46fc4035c1fab916f1
Tobias Brandy <tbrandy@itba.edu.ar>
2022-07-11 22:01:52 -0300

    Exception handling

--- pf_filament_tracking.py
+++ pf_filament_tracking.py
@@ -4,17 +4,11 @@ from typing import List, Dict, Type
 
 import numpy as np
 from flask import render_template, request, make_response, jsonify, Flask
+from werkzeug.exceptions import HTTPException
 
 from tracking.image_utils import frames_iterator
 from tracking.main import track_filament
 from tracking.models import Config, ApplicationError
-
-ALLOWED_IMAGE_TYPES: List[str] = ['.tif', '.tiff', '.jpg', '.jpeg', '.avi', '.png']
-TYPE_TO_INPUT: Dict[Type, str] = {
-    bool:   'checkbox',
-    int:    'text',
-    float:  'text'
-}
 
 app = Flask(
     __name__,
@@ -24,10 +18,29 @@ app = Flask(
 )
 # app.secret_key = 'secret key'
 
+ALLOWED_IMAGE_TYPES: List[str] = ['.tif', '.tiff', '.jpg', '.jpeg', '.avi', '.png']
+TYPE_TO_INPUT: Dict[Type, str] = {
+    bool:   'checkbox',
+    int:    'text',
+    float:  'text'
+}
+
 @app.after_request
 def add_header(response):
     response.headers["Cache-Control"] = "max-age=0, must-revalidate"
     return response
+
+@app.errorhandler(ApplicationError)
+def application_error_handler(e):
+    return make_response(jsonify(e), 400)
+
+@app.errorhandler(HTTPException)
+def application_error_handler(e):
+    return make_response(jsonify(ApplicationError(str(e)), e.code))
+
+@app.errorhandler(Exception)
+def generic_error_handler(e):
+    return make_response(jsonify(ApplicationError(str(e))), 500)
 
 @app.route('/')
 def index():
@@ -41,19 +54,14 @@ def index():
 @app.route('/track', methods=['POST'])
 def track():
     if 'points' not in request.form or len(points := json.loads(request.form['points'], object_hook=lambda point: (point['x'], point['y']))) < 2:
-        return make_response(jsonify(ApplicationError('No enough points provided for tracking. At least 2 are required.')), 400)
+        raise ApplicationError('No enough points provided for tracking. At least 2 are required.')
 
     if 'images[]' not in request.files or len(images := request.files.getlist('images[]')) < 1:
-        return make_response(jsonify(ApplicationError('No images provided for tracking. At least one image is required.')), 400)
+        raise ApplicationError('No images provided for tracking. At least one image is required.')
 
     config = Config.from_dict(request.form)
 
-    try:
-        return make_response(jsonify(track_filament(frames_iterator(images, ALLOWED_IMAGE_TYPES), np.array(points), config)))
-    except ApplicationError as e:
-        return make_response(jsonify(e), 400)
-    except Exception as e:
-        return make_response(jsonify(ApplicationError(str(e))), 500)
+    return make_response(jsonify(track_filament(frames_iterator(images, ALLOWED_IMAGE_TYPES), np.array(points), config)))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
