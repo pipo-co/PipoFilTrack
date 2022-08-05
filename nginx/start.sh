1088a29ab013ff07eb907eff37b625f0663aeae8
Ignacio Sagues <isagues@itba.edu.ar>
2022-08-05 12:11:57 -0300

    https

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-envsubst '$APP_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst '$APP_ADDR $SERVER_NAME' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
