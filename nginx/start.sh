12cf4c42053f85cced665f4afcae82281f0aabcb
Ignacio Sagues <isagues@itba.edu.ar>
2022-08-05 21:57:12 -0300

    default.conf rename

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-envsubst '$APP_ADDR $SERVER_NAME' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst '$APP_ADDR SERVER_NAME' < /tmp/app.conf > /etc/nginx/conf.d/app.conf && nginx -g 'daemon off;'
