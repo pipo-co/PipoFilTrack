f8bb34538bfd8db46d5576a42c8d24aad2ca152b
Tobias Brandy <tbrandy@itba.edu.ar>
2022-08-21 17:30:36 -0300

    results.js: control is object

--- static/js/results.js
+++ static/js/results.js
@@ -7,33 +7,34 @@ class ResultsViewer {
         this.frames     = [];
         this.index      = 0;
         this.inter_id   = null;
-        this.fps        = 1;
+        this.fps        = 3;
 
-        this.controls = {};
+        this.controls = null;
     }
 
     bindViewer(bindElement) {
         bindElement.appendChild(this.canvas);
 
-        // TODO(tobi): Porque deep copy?
         const template = document.getElementById(this.templateId);
         bindElement.appendChild(template.content.cloneNode(true));
 
-        this.controls['frame']              = document.getElementById('rc-frame');
-        this.controls['play']               = document.getElementById('rc-play');
-        this.controls['stop']               = document.getElementById('rc-stop');
-        this.controls['next_frame']         = document.getElementById('rc-next_frame');
-        this.controls['prev_frame']         = document.getElementById('rc-prev_frame');
-        this.controls['frame_rate_up']      = document.getElementById('rc-frame_rate_up');
-        this.controls['frame_rate_down']    = document.getElementById('rc-frame_rate_down');
-        this.controls['frame_rate']         = document.getElementById('rc-frame_rate_value');
+        this.controls = {
+            frame:              document.getElementById('rc-frame'),
+            play:               document.getElementById('rc-play'),
+            stop:               document.getElementById('rc-stop'),
+            nextFrame:          document.getElementById('rc-next_frame'),
+            prevFrame:          document.getElementById('rc-prev_frame'),
+            frameRateUp:        document.getElementById('rc-frame_rate_up'),
+            frame_rate_down:    document.getElementById('rc-frame_rate_down'),
+            frame_rate:         document.getElementById('rc-frame_rate_value'),
+        };
 
-        this.controls['play']           .addEventListener('click', () => this.resumeAnimation());
-        this.controls['stop']           .addEventListener('click', () => this.stopAnimation());
-        this.controls['next_frame']     .addEventListener('click', () => this.drawNextFrame());
-        this.controls['prev_frame']     .addEventListener('click', () => this.drawPrevFrame());
-        this.controls['frame_rate_up']  .addEventListener('click', () => this.updateFrameRate(1));
-        this.controls['frame_rate_down'].addEventListener('click', () => this.updateFrameRate(-1));
+        this.controls.play              .addEventListener('click', () => this.resumeAnimation());
+        this.controls.stop              .addEventListener('click', () => this.stopAnimation());
+        this.controls.nextFrame         .addEventListener('click', () => this.drawNextFrame());
+        this.controls.prevFrame         .addEventListener('click', () => this.drawPrevFrame());
+        this.controls.frameRateUp       .addEventListener('click', () => this.updateFrameRate(1));
+        this.controls.frame_rate_down   .addEventListener('click', () => this.updateFrameRate(-1));
     }
 
     loadResults(frames) {
