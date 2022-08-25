d7e72bf247a5e54cfa5c594bdc148a58c8e42b56
Ignacio Sagues <isagues@itba.edu.ar>
2022-08-25 11:26:18 -0300

    models.py: order config based on type

--- tracking/models.py
+++ tracking/models.py
@@ -22,10 +22,10 @@ def float_config_field(default: float, name: str, desc: str, step: float, min_: 
 class Config:
     max_fitting_error: float    = float_config_field(0.2, 'Tolerancia de error', 'Limite de tolerancia en el error del ajuste gaussiano del perfil de intensidad', step=0.01, min_=0, max_=100)
     normal_line_length: int     = int_config_field(10, 'Longitud del perfil de intensidad', 'Longitud en pixeles del perfil de intensidad a tomar perpendicularmente a cada punto', min_=2, max_=100)
-    bezier_smoothing: bool      = bool_config_field(True, 'Suavizado final', 'Post-procesamiento de suavizado del filamento ajustando a una curva de Bezier')
     point_density: int          = int_config_field(1, 'Densidad de puntos', 'Proporción de puntos a usar durante la interpolación por pixel (1/n). La máxima densidad es 1. Reducir la densidad reduce la precisión, pero aumenta la velocidad del algoritmo', min_=1, max_=100)
     missing_inter_len: int      = int_config_field(3, 'Cantidad de puntos para interpolar', 'Cantidad de puntos vecinos a tomar hacia ambos lados para interpolar los puntos considerados inválidos (rojos)', min_=1, max_=20)
     max_tangent_length: int     = int_config_field(15, 'Puntos para calcular tangente', 'Cantidad de puntos vecinos tomados para calcular la pendiente de cada punto', min_=1, max_=100)
+    bezier_smoothing: bool      = bool_config_field(True, 'Suavizado final', 'Post-procesamiento de suavizado del filamento ajustando a una curva de Bezier')
 
     @classmethod
     def from_dict(cls, env):
