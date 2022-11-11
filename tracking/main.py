b273b25efab60edca0613d4af6cb38bb700b976d
Tobias Brandy <tbrandy@itba.edu.ar>
2022-11-11 11:57:00 -0300

    Typos in comments

--- tracking/main.py
+++ tracking/main.py
@@ -15,7 +15,7 @@ def track_filament(frames: Iterable[np.ndarray], user_points: np.ndarray, config
     # Obtenemos los puntos iniciales del tracking interpolando linealmente los puntos del usuario
     prev_frame_points = multi_point_linear_interpolation(user_points, config.point_density)
 
-    # Si no hay suficientes puntos para la tangente configurada, bajamos la cantidada de puntos
+    # Si no hay suficientes puntos para la tangente configurada, bajamos la cantidad de puntos
     max_tangent_length = min(config.max_tangent_length, len(prev_frame_points) - 1)
 
     for frame in frames:
@@ -23,28 +23,28 @@ def track_filament(frames: Iterable[np.ndarray], user_points: np.ndarray, config
         if config.inverted:
             frame = 255 - frame
 
-        # Calculamos los limites que definen los segmentos de las rectas normales
+        # Calculamos los límites que definen los segmentos de las rectas normales
         normal_lines_limits = generate_normal_line_bounds(prev_frame_points, max_tangent_length, config.normal_line_length)
 
-        # A partir de los limites obtenemos la lista de pixeles que representan a los segmentos de las rectas normales
+        # A partir de los límites obtenemos la lista de píxeles que representan a los segmentos de las rectas normales
         # No es un ndarray porque no todas salen con la misma longitud (diagonales, etc)
         normal_lines = [points_linear_interpolation(start, end) for start, end in normal_lines_limits]
 
         # Obtenemos los perfiles de intensidad de la imagen de cada recta normal
         intensity_profiles = map(lambda nl, img=frame: read_line_from_img(img, nl), normal_lines)
 
-        # Obtenemos la posicion del maximo punto del perfil de intensidad.
+        # Obtenemos la posición del máximo punto del perfil de intensidad.
         # Puede retornar None en caso de que no se pueda fittear la curva de intensidad, o si el error es mayor al maximo permitido.
         points_profile_pos = list(map(lambda ip, img=frame: gauss_fitting(ip, config.max_fitting_error), intensity_profiles))
 
         # A partir de las posiciones, obtenemos los puntos que representan.
-        # En caso de que el error de la posicion fuese muy alto,
-        #  o la posicion no estuviese dentro del perfil de intesidad, obtenemos None en vez del punto.
+        # En caso de que el error de la posición fuese muy alto,
+        #  o la posición no estuviese dentro del perfil de intensidad, obtenemos None en vez del punto.
         raw_points_with_missing = [profile_pos_to_point(pos, nl) if pos else None for pos, nl in zip(points_profile_pos, normal_lines)]
 
-        # Buscamos llenar los valores faltantes (None) mediante una interpolacion con los vecinos bien calculados.
-        # En caso de que la interpolacion no pueda ser hecha, se descartan los valores.
-        # Se informa la posicion de los valores interpolados o descartados.
+        # Buscamos llenar los valores faltantes (None) mediante una interpolación con los vecinos bien calculados.
+        # En caso de que la interpolación no pueda ser hecha, se descartan los valores.
+        # Se informa la posición de los valores interpolados o descartados.
         raw_points, interpolated_points, preserved_points = interpolate_missing(raw_points_with_missing, prev_frame_points, config.missing_inter_len)
 
         # Si fue seleccionado, suavizamos los puntos ajustando los mismos a una curva de bezier
@@ -56,7 +56,7 @@ def track_filament(frames: Iterable[np.ndarray], user_points: np.ndarray, config
             errors.append(str(e))
             smoothed_points = raw_points
 
-        # Ya obtuvimos los puntos finales del frame! Los disponibilizamos como los puntos iniciales del proximo frame
+        # Ya obtuvimos los puntos finales del frame! Los disponibilizamos como los puntos iniciales del próximo frame
         prev_frame_points = smoothed_points
 
         # Guardamos el resultado del frame
